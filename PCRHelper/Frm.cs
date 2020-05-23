1c4a8e03dfb2243092cf688c56f43d44f1fc092e
Koishi <yucong@lilith.sh>
2020-05-23 21:58:05 +0800

    202005232157

---
+++ PCRHelper/Frm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PCRHelper
+{
+    public partial class Frm : Form
+    {
+        public Frm()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Load(object sender, EventArgs e)
+        {
+            var tools = new Tools();
+            var proc = tools.GetMumuProcess();
+        }
+    }
+}
---
+++ PCRHelper/Tools.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace PCRHelper
+{
+    class Tools
+    {
+        /// <summary>
+        /// 获取Mumu模拟器进程
+        /// </summary>
+        /// <returns></returns>
+        public Process GetMumuProcess()
+        {
+            var processes = Process.GetProcesses();
+            foreach (var process  in processes)
+            {
+                var procName = process.ProcessName;
+                if (procName.Contains("Nemu"))
+                {
+
+                }
+            }
+            return null;
+        }
+    }
+}
